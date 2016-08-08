8cca88150dfad9dd7a8c2171cc56b7715cd335f9
Thien Tran <thientran1986@gmail.com>
2016-08-08 22:44:11 +0700

    Minor change

--- src/EspMQTTSketch.cpp
+++ src/EspMQTTSketch.cpp
@@ -33,11 +33,13 @@ void EspMQTTSketch::setup(){
 
 void EspMQTTSketch::loop(){
   // put your main code here, to run repeatedly:
+  ArduinoOTA.handle();
+
   if (!this->client->connected()) {
     this->reconnect();
   }
   this->client->loop();
-  delay(500);
+  delay(200);
 }
 
 void EspMQTTSketch::reconnect(){
@@ -65,6 +67,7 @@ void EspMQTTSketch::initializeWifi(){
   Serial.print("Connecting to ");
   Serial.println(this->wifiSSID);
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(this->wifiSSID.c_str(), this->wifiPassword.c_str());
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -73,10 +76,34 @@ void EspMQTTSketch::initializeWifi(){
     ESP.restart();
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  ArduinoOTA.onStart([]() {
+   String type;
+   if (ArduinoOTA.getCommand() == U_FLASH)
+     type = "sketch";
+   else // U_SPIFFS
+     type = "filesystem";
+
+   // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+   Serial.println("Start updating " + type);
+ });
+ ArduinoOTA.onEnd([]() {
+   Serial.println("\nEnd");
+ });
+ ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+ });
+ ArduinoOTA.onError([](ota_error_t error) {
+   Serial.printf("Error[%u]: ", error);
+   if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+   else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+   else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+   else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+   else if (error == OTA_END_ERROR) Serial.println("End Failed");
+ });
+ ArduinoOTA.begin();
+ Serial.println("Ready");
+ Serial.print("IP address: ");
+ Serial.println(WiFi.localIP());
 }
 
 void EspMQTTSketch::initializeEspPubSubClient(){
